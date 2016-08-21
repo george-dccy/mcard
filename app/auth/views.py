da356abbe414bab6e21320f07e2a9b5ac88c8c0a
George Liu <george.dccy@gmail.com>
2016-08-21 13:33:03 +0800

    little modification.

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,7 +13,7 @@ from flask import jsonify
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter(User.username==form.username.data).filter(User.active_flag!=-1).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(url_for('main.index'))
