a8f381074a73642347530c39743c0feb48b78883
George Liu <george.dccy@gmail.com>
2016-08-28 09:30:27 +0800

    Revert "修改自动加载"
    This reverts commit 16cfad56a7ed785c0278645f315d0c6267d72458.

--- app/admin/views.py
+++ app/admin/views.py
@@ -281,9 +281,9 @@ def alter_card():
     pass
 
 
-@admin.route('/delete_card/<int:card_id>', methods=['GET', 'POST'])
+@admin.route('/delect_card/<int:card_id>', methods=['GET', 'POST'])
 @admin_required
-def delete_card(card_id):
+def delect_card(card_id):
     thiscard = Card.query.filter_by(id=card_id).first()
     if thiscard:
         db.session.delete(thiscard)
